bd9ee9fee7c8b3612b84ecf219616b3a18d785ff
Artur Kot <arturkot@onmail.com>
2022-04-23 19:03:25 +0000

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,13 +5,15 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
-        <Head />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Coiny&family=Fira+Code&family=Fira+Sans:ital,wght@0,200;0,400;0,600;0,800;1,200;1,400;1,500;1,600;1,800&family=Fredericka+the+Great&display=swap"
-          rel="stylesheet"
-        />
+        <Head>
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Coiny&family=Fira+Code&family=Fira+Sans:ital,wght@0,200;0,400;0,600;0,800;1,200;1,400;1,500;1,600;1,800&family=Fredericka+the+Great&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
 
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
